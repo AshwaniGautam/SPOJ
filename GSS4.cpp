71fa9f865667f4d181fdafc99e1ddc28f12dee26
Ashwani Gautam <lwmarsh06@gmail.com>
2016-09-23 11:25:47 +0530

    Solution ACed

--- GSS4.cpp
+++ GSS4.cpp
@@ -1,77 +1,93 @@
-#include<stdio.h>
-#include<iostream>
-#include<math.h>
-#include<string.h>
-using namespace std;
-int array[100009]   ;
-int ST[500005], flag[100009]  ;
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <bitset>
+#include <cmath>
 
-void update(int index, int low, int high, int pos, int inv){
-    if (index > high || index < low) return     ;
+using namespace std;
+
+long long int array[100009]   ;
+long long int ST[262150]  ;
+
+void build(int low, int high, int pos){
+
+    if (low == high){
+        
+        ST[pos] = array[low]    ;
+        return;
+
+    }
+
+    int mid = (low+high) / 2    ;
+    build(low, mid, 2*pos+1)    ;
+    build(mid+1, high, 2*pos+2) ;
+
+    ST[pos] = ST[2*pos+1] + ST[2*pos+2] ;
+
+    return;
+}
+
+void update(int L, int R, int low, int high, int pos){
+
+    if ( low > R || high < L)
+         return     ;
+
+    if (ST[pos] == (high - low + 1))
+        return  ;
+
     if(low == high){
-        if (inv){
-            ST[pos] = array[index]  ;
-        }
-        else{
-            ST[pos] = int(pow(ST[pos], 0.5)) ;
-            if (ST[pos] == 1)
-                flag[index] = 0;
-        }
+
+        ST[pos] = sqrt(ST[pos]) ;
         return;
     }
 
     int mid = (low+high) / 2    ;
-    update(index, low, mid, 2*pos+1, inv)    ;
-    update(index, mid+1, high, 2*pos+2, inv) ;
-    ST[pos] = ST[2*pos+1] + ST[2*pos+2] ;
+    update(L, R, low, mid, 2*pos+1) ;
+    update(L, R, mid+1, high, 2*pos+2)  ;
+
+    ST[pos]  = ST[2*pos+1] +  ST[2*pos+2]   ;
+
     return ;
 }
 
 long long int query(int i, int j, int low, int high, int pos){
+
     if (high < i || low > j)
         return 0;
+
     if (i <= low && high <= j)
         return ST[pos]  ;
+
     int mid  = (low+high) /  2  ;
-    long long int a = query(i, j, low, mid, 2*pos+1)    ;
-    long long int b = query(i, j, mid+1, high, 2*pos+2)  ;
-    long long int c = a + b;
-    return c    ;
+    return(query(i, j, low, mid, 2*pos+1) + query(i, j, mid+1, high, 2*pos+2))  ;
+
 }
 
 int main(){
-    int N, L, R, M, type, test = 0   ;
-    while(scanf("%d", &N) != 0){
-        test++  ;
-        memset(ST, 0, sizeof ST)    ;
-        memset(array, 0, sizeof ST) ;
-        memset(flag, 1, sizeof flag)    ;
-        for (int i = 0; i < N; i++){
-            scanf("%d", &array[i])  ;
-            update(i, 0, N-1, 0, 1) ;
-        }
-        scanf("%d", &M);
-        /*for (int i = 0; i < 17; i++)
-            cout << ST[i] << " "    ;
-        for (int i = 0; i < 8; i++)
-            cout << array[i] << " " ;
-        cout << endl;*/
-        printf("Case #%d:\n", test) ;
-        while(M--){
-            scanf("%d %d %d", &type, &L, &R)  ;
-            L = min(L, R)   ;
-            R = max(L, R)   ;
-            if (!type)
-                for (int i = L-1; i < R; i++){
-                    if (flag[i]){
-                        update(i, 0, N-1, 0, 0)    ;
-                    }
-                }
-            else{
-                printf("%lld\n", query(L-1, R-1, 0, N-1, 0))    ;
+
+        ios_base::sync_with_stdio(false);   cin.tie(0)  ;
+        int N, L, R, M, type, test = 0   ;
+        while(scanf("%d", &N) != EOF){
+
+            test++  ;
+            memset(ST, 0, sizeof ST)    ;
+            for (int i = 0; i < N; i++)
+                scanf("%lld", &array[i])  ;
+            build(0, N - 1, 0)  ;
+            scanf("%d", &M);
+
+            printf("Case #%d:\n", test) ;
+            while(M--){
+                scanf("%d %d %d", &type, &L, &R)  ;
+
+                if (!type)
+                    update(min(L, R) - 1, max(L, R) - 1, 0, N-1, 0)    ;
+
+                else
+                    printf("%lld\n", query(min(L, R) - 1, max(L, R) - 1, 0, N-1, 0))    ;
             }
-        }
-        printf("\n")    ;
+            printf("\n")    ;
     }
     return 0    ;
 }
