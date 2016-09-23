02a461f4b8348c20d9e59c0a1b4f87e129250953
Ashwani Gautam <lwmarsh06@gmail.com>
2016-09-23 11:27:45 +0530

    Minor Changes

--- MULTQ3.cpp
+++ MULTQ3.cpp
@@ -1,16 +1,16 @@
 /*
-The darkest places in the earth are reserved for those who maintain thier neutrality in the time of moral crisis- Bertrand Zobrist
+The darkest places in the hell are reserved for those who maintain thier neutrality in the time of moral crisis- Bertrand Zobrist
 Religion and GOD are the only shit between India and Development.
 */
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
 
 struct node{
     int mod_1, mod_2, ans, lazy   ;
 };
 
-node tree[400009]   ;
+node tree[262150]   ;
 
 void build(int low, int high, int pos){
     if (high == low){
@@ -20,13 +20,16 @@ void build(int low, int high, int pos){
         tree[pos].lazy =   0;
         return ;
     }
+
     int mid = (low+high) / 2    ;
     build(low, mid, 2*pos+1)  ;
     build(mid+1, high, 2*pos+2);
+
     tree[pos].ans = high - low + 1  ;
     tree[pos].mod_1 = 0 ;
     tree[pos].mod_2 = 0 ;
-    tree[pos].lazy =   0;
+    tree[pos].lazy  = 0 ;
+
     return;
 }
 
@@ -65,13 +68,16 @@ void update(int i, int j, int low, int high, int pos){
             }
         return;
     }
+    
     int mid = (low+high) / 2    ;
     update(i, j, low, mid, 2*pos+1) ;
     update(i, j, mid+1, high, 2*pos+2)  ;
+
     tree[pos].lazy = 0  ;
     tree[pos].mod_1 = tree[2*pos+1].mod_1 + tree[2*pos+2].mod_1 ;
     tree[pos].mod_2 = tree[2*pos+1].mod_2 + tree[2*pos+2].mod_2 ;
     tree[pos].ans = tree[2*pos+1].ans + tree[2*pos+2].ans ;
+    
     return  ;
 }
 
@@ -85,21 +91,29 @@ int query(int i, int j, int low, int high, int pos){
         }
         else
             if(tree[pos].lazy % 3 == 2){
+
                 int swap = tree[pos].mod_1    ;
                 tree[pos].mod_1 = tree[pos].mod_2 ;
                 tree[pos].mod_2 = tree[pos].ans   ;
                 tree[pos].ans = swap;
+
             }
         if (high != low){
+
             tree[2*pos+1].lazy += tree[pos].lazy;
             tree[2*pos+2].lazy += tree[pos].lazy;
+
         }
+
         tree[pos].lazy = 0  ;
     }
+
     if (low > j || high < i)
         return 0;
+
     if (i <= low && high <= j)
         return tree[pos].ans    ;
+
     int mid = (low+high) / 2    ;
     return (query(i, j, low, mid, 2*pos+1)  +    query(i, j, mid+1, high, 2*pos+2));
 }
